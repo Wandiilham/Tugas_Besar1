1fb2ef8d2de9d32cf21eea3d1e9250e229932c8d
Wandi ilham <wandi.i@student.unsil.ac.id>
2014-05-05 07:16:17 +0700

    Create about.php

---
+++ about.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Perpustakaan-online</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div id="konten">
+ <div id="head">
+  <div id="logo">
+  </div>
+   <div id="nav">
+    <ul class="nav-link">
+       <li><a href="index.php" title="home">home</a></li>
+       <li id="acitve-nav"><a href="about.php" title="about">about</a></li>
+       <li class="border-right"><a href="contact.php" title="contact">contact</a></li>
+    </ul>
+   </div>
+ </div> 
+ <div id="welcome">
+  <div id="welcome-text">
+
+Alhamdulilah kelompok kami bisa menyelsaikan tugas yang di berikan salah satu dosen kami meski banyak kekeurangan dalam proses pengerjaan nya mohon maap atas segala kekurangnya karena kami dalam proses belajar.
+ada sedikit yang di selipkan di about ini mengenai kata-kata bijak yang berkata "<i>Belajar memang melelahkan namun lebih lelah nanti jika kalau saat ini tidak belajar</i>"maka dari itu terimakasih kepada dosen kami yang tak pernah lelah untuk memberi materi pembelajaran  buat kita semua.
+</div>
+   <div id="welcome-image">  
+   <img src="img/bendera.jpg" align="right" width="300" height="400" />
+  </div>
+  </div>    
+ </div>  
+ <div class="line">&nbsp;</div>
+ 
+
+ <div id="footer">
+  <div id="footer-link">
+  <a href="index.php">home</a> | <a href="about.php">about</a> | <a href="contact.php">contact</a>  
+  </div>
+  <div id="footer-copy">
+  <p>copyright Upi latipudin | Helmy Pratama Tryas Rustandy | Wandi ilham | Rizal muhamad noor </p>
+  </div>
+ </div>
+</div>
+</body>
+</html>
