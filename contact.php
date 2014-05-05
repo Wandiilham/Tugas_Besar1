7efb251464fa4a3b19b35581e5dee87b9efdf2b6
Wandi ilham <wandi.i@student.unsil.ac.id>
2014-05-05 07:13:32 +0700

    saya namabh file contact

---
+++ contact.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Perpustakaan-online</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div id="konten">
+ <div id="head">
+  <div id="logo">
+  </div>
+   <div id="nav">
+    <ul class="nav-link">
+       <li><a href="index.php" title="home">home</a></li>
+       <li ><a href="about.php" title="about">about</a></li>
+       <li class="border-right" id="acitve-nav"><a href="contact.php" title="contact">contact</a></li>
+    </ul>
+   </div>
+ </div> 
+ <div id="welcome">
+  <div id="welcome-text">
+  <ul class="contact">
+   <li class="phone">082216598040</li>
+   <li class="mail">hhtps:/github/Viiemultazam</li>
+   <li class="phone">085721666192</li>
+   <li class="mail">hhtps:/github/Helmypratama</li>
+    <li class="phone">083816953613</li>
+   <li class="mail">hhtps:/github/Wandiilham</li>
+    <li class="phone">085794720809</li>
+   <li class="mail">hhtps:/github/RizalMnoer</li>
+  </ul> 
+  </div>    
+  <div i
+ 
+ <div id="footer">
+  <div id="footer-link">
+  <a href="index.php">home</a> | <a href="about.php">about</a> | <a href="contact">contact</a>  
+  </div>
+  <div id="footer-copy">
+   <p>copyright Upi latipudin | Helmy Pratama Tryas Rustandy | Wandi ilham | Rizal muhamad noor </p>
+  </div>
+ </div>
+</div>
+</body>
+</html>
